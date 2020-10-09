d4636cf178849838e3f02fa51eb1356dcd0b7afa
Laxmi Paudel <meluxmeme@gmail.com>
2020-10-08 20:33:01 -0500

    Update PlotMesh.m
    added the option to choose colormap.

--- PlotMesh.m
+++ PlotMesh.m
@@ -1,27 +1,27 @@
 # matlab codes to plot mesh
-function PlotMesh(coordinates,nodes)
-nel = length(nodes) ;                  % number of elements
-nnode = length(coordinates) ;          % total number of nodes in system
+function PlotMesh(coordinates,nodes,colors)
+Xnodes= length(nodes) ;                  % number of elements
+Ynodes = length(coordinates) ;          % total number of nodes in system
 nnel = size(nodes,2);                % number of nodes per element
 % 
 % Initialization of the required matrices
+nel=Xnodes;
+nnel=Ynodes;
 X = zeros(nnel,nel) ;
 Y = zeros(nnel,nel) ;
-
 for iel=1:nel   
      for i=1:nnel
      nd(i)=nodes(iel,i);         % extract connected node for (iel)-th element
      X(i,iel)=coordinates(nd(i),1);    % extract x value of the node
      Y(i,iel)=coordinates(nd(i),2);    % extract y value of the node
      end
-end
-    
+end   
 % Plotting the FEM mesh, diaplay Node numbers and Element numbers
      f1 = figure ;
      set(f1,'name','Mesh','numbertitle','off') ;
      plot(X,Y,'k')
      %fill(X,Y,'w')
-     colormap(jet);
+     colormap(colors);
      title('Finite Element Mesh') ;
      axis off ;
      k = nodes(:,1:end);
